1f0219845443a702648cf34ef10187a4002647be
Franccesco Orozco <franccesco@thatai.dev>
2023-11-11 13:15:18 -0600

    Add function to get meeting issues

--- lib/bloomy/client.rb
+++ lib/bloomy/client.rb
@@ -38,5 +38,21 @@ module Bloomy
       response = @conn.get("L10/#{meeting_id}/attendees").body
       attendees = response.map { |attendee| { name: attendee['Name'], id: attendee['Id'] } }
     end
+
+    def get_meeting_issues(meeting_id, include_closed = false)
+      response = @conn.get("L10/#{meeting_id}/issues?include_resolved=#{include_closed}").body
+      issues = response.map do |issue|
+        {
+          id: issue['Id'],
+          title: issue['Name'],
+          created_at: issue['CreateTime'],
+          closed_at: issue['CloseTime'],
+          owner: {
+            id: issue['Owner']['Id'],
+            name: issue['Owner']['Name']
+          }
+        }
+      end
+    end
   end
 end
--- spec/bloomy_spec.rb
+++ spec/bloomy_spec.rb
@@ -69,5 +69,21 @@ RSpec.describe Bloomy do
     it "returns the attendees of a meeting" do
       attendees = client.get_meeting_attendees(meeting_id)
     end
+
+    it "returns the issues attached to a meeting" do
+      issues = client.get_meeting_issues(meeting_id, include_closed = true)
+      expect(issues).to include(
+        {
+          id: a_kind_of(Integer),
+          title: a_kind_of(String),
+          created_at: a_kind_of(String),
+          closed_at: a_kind_of(String).or(be_nil),
+          owner: {
+            id: a_kind_of(Integer),
+            name: a_kind_of(String),
+          }
+        }
+      )
+    end
   end
 end
