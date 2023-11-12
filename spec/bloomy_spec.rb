2b5c10704036eade452390abd51c2e38ac6aeb95
Franccesco Orozco <franccesco@thatai.dev>
2023-11-11 22:21:55 -0600

    Create feat. to get archived rocks

--- spec/bloomy_spec.rb
+++ spec/bloomy_spec.rb
@@ -85,5 +85,31 @@ RSpec.describe Bloomy do
         }
       )
     end
+
+    it "returns my rocks" do
+      rocks = client.get_my_rocks
+      expect(rocks).to include(
+        {
+          id: a_kind_of(Integer),
+          title: a_kind_of(String),
+          created_at: a_kind_of(String),
+          due_date: a_kind_of(String),
+          status: eq('Completed').or(eq('Incomplete'))
+        }
+      )
+    end
+
+    it "returns my archived rocks" do
+      archived_rocks = client.get_my_archived_rocks
+      expect(archived_rocks).to include(
+        {
+          id: a_kind_of(Integer),
+          title: a_kind_of(String),
+          created_at: a_kind_of(String),
+          due_date: a_kind_of(String),
+          status: eq('Completed').or(eq('Incomplete'))
+        }
+      )
+    end
   end
 end
