0ba93bcbc08e8bdbc8043a8b1e1b19894614e2c2
Franccesco Orozco <franccesco@thatai.dev>
2023-11-14 21:18:17 -0600

    Add headers required to interact with the API

--- lib/bloomy/client.rb
+++ lib/bloomy/client.rb
@@ -20,7 +20,8 @@ module Bloomy
       @conn = Faraday.new(url: @base_url) do |faraday|
         faraday.response :json
         faraday.adapter Faraday.default_adapter
-        faraday.headers['Accept'] = 'application/json'
+        faraday.headers['Accept'] = '*/*'
+        faraday.headers['Content-Type'] = 'application/json'
         faraday.headers['Authorization'] = "Bearer #{configuration.api_key}"
       end
       @user_id = nil
