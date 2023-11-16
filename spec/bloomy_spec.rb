847e6b3d6bf17a1cb18109d422b33ea8ebe8d12f
Franccesco Orozco <franccesco@thatai.dev>
2023-11-16 12:06:54 -0600

    Add tests for issues functionality

--- spec/bloomy_spec.rb
+++ spec/bloomy_spec.rb
@@ -153,5 +153,35 @@ RSpec.describe Bloomy do
       should_return_true = client.update_current_scorecard(id_to_update, 3.0)
       expect(should_return_true).to be true
     end
+
+    it "returns my issues", :vcr do
+      my_issues = client.get_my_issues
+      expect(my_issues).to include(
+        {
+          id: a_kind_of(Integer),
+          title: a_kind_of(String),
+          notes_url: a_kind_of(String),
+          created_at: a_kind_of(String),
+          completed_at: a_kind_of(String).or(be_nil),
+          meeting_id: a_kind_of(Integer),
+          meeting_name: a_kind_of(String),
+        }
+      )
+    end
+
+    it "creates and completes an issue", :vcr do
+      # Create an issue
+      created_issue = client.create_issue("Test issue", meeting_id)
+      expect(created_issue).to include(
+        {
+          id: a_kind_of(Integer),
+          title: a_kind_of(String),
+        }
+      )
+
+      # Complete the created issue
+      completed_issue = client.complete_issue(created_issue[:id])
+      expect(completed_issue).to be true
+    end
   end
 end
