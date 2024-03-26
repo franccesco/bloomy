29af4359e0e1a30d65f1c41bccdca083bffc2aa3
Franccesco Orozco <franccesco@codingdose.info>
2024-03-25 18:24:50 -0600

    Update test cases on meeting ops

--- spec/bloomy_meetings_spec.rb
+++ spec/bloomy_meetings_spec.rb
@@ -1,36 +1,39 @@
-RSpec.describe "Meeting Operations" do
+# FILEPATH: /Users/fran/workspace/bloomy/spec/bloomy_meeting_operations_spec.rb
+
+RSpec.describe Bloomy::MeetingOperations do
   let(:client) { Bloomy::Client.new }
-  let(:meeting_id) { ENV['MEETING_ID'] }
+  let(:user_id) { client.get_my_user_id }
+  let(:meeting_id) { client.get_meetings(user_id: user_id).first[:id] }
 
-  context "when interacting with meetings API", :vcr do
-    it "returns the meetings visible to the user", :vcr do
-      meetings = client.get_meetings
-      expect(meetings).to include(
-        {
-          id: a_kind_of(Integer),
-          name: a_kind_of(String),
-        }
-      )
+  context "when interacting with meetings API" do
+    it "returns a list of meetings" do
+      meetings = client.get_meetings(user_id: user_id)
+      expect(meetings).to all(include(:id, :name))
     end
 
-    it "returns the attendees of a meeting", :vcr do
+    it "returns a list of meeting attendees" do
       attendees = client.get_meeting_attendees(meeting_id)
+      expect(attendees).to all(include(:name, :id))
     end
 
-    it "returns the issues attached to a meeting", :vcr do
-      issues = client.get_meeting_issues(meeting_id, include_closed = true)
-      expect(issues).to include(
-        {
-          id: a_kind_of(Integer),
-          title: a_kind_of(String),
-          created_at: a_kind_of(String),
-          closed_at: a_kind_of(String).or(be_nil),
-          owner: {
-            id: a_kind_of(Integer),
-            name: a_kind_of(String),
-          }
-        }
-      )
+    it "returns a list of meeting issues" do
+      issues = client.get_meeting_issues(meeting_id)
+      expect(issues).to all(include(:id, :title, :created_at, :closed_at, :details_url, :owner))
+    end
+
+    it "returns a list of meeting todos" do
+      todos = client.get_meeting_todos(meeting_id)
+      expect(todos).to all(include(:id, :title, :due_date, :details_url, :completed_at, :owner))
+    end
+
+    it "returns a list of meeting measurables" do
+      measurables = client.get_meeting_measurables(meeting_id)
+      expect(measurables).to all(include(:id, :name, :target, :operator, :format, :owner, :admin))
+    end
+
+    it "returns meeting details" do
+      details = client.get_meeting_details(meeting_id)
+      expect(details).to include(:id, :name, :attendees, :issues, :todos, :measurables)
     end
   end
 end
