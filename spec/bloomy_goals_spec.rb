7575f1dedf56fd70a6819f5b058922a6064c94c6
Franccesco Orozco <franccesco@codingdose.info>
2024-11-03 17:12:21 -0600

    Rename rocks to goals

--- spec/bloomy_goals_spec.rb
+++ spec/bloomy_goals_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-RSpec.describe "Rock Operations" do
+RSpec.describe "Goal Operations" do
   before(:all) do
     @client = Bloomy::Client.new
     @user_id = @client.user.default_user_id
     @meeting_details = @client.meeting.create(title: "Test Meeting")
-    @created_rock = @client.rock.create(title: "Test Rock", user_id: @user_id,
+    @created_goal = @client.goal.create(title: "Test Goal", user_id: @user_id,
       meeting_id: @meeting_details[:meeting_id])
   end
 
@@ -13,10 +13,10 @@ RSpec.describe "Rock Operations" do
     @client.meeting.delete(@meeting_details[:meeting_id])
   end
 
-  context "when interacting with rocks API" do
-    it "returns user rocks" do
-      rocks = @client.rock.list(user_id: @user_id)
-      expect(rocks).to include(
+  context "when interacting with goals API" do
+    it "returns user goals" do
+      goals = @client.goal.list(user_id: @user_id)
+      expect(goals).to include(
         {
           id: a_kind_of(Integer),
           title: a_kind_of(String),
@@ -29,9 +29,9 @@ RSpec.describe "Rock Operations" do
       )
     end
 
-    it "returns user active & archived rocks" do
-      rocks = @client.rock.list(user_id: @user_id, archived: true)
-      expect(rocks).to include(
+    it "returns user active & archived goals" do
+      goals = @client.goal.list(user_id: @user_id, archived: true)
+      expect(goals).to include(
         {
           active: a_kind_of(Array),
           archived: a_kind_of(Array)
@@ -39,10 +39,10 @@ RSpec.describe "Rock Operations" do
       )
     end
 
-    it "tests the created rock" do
-      expect(@created_rock).to include(
+    it "tests the created goal" do
+      expect(@created_goal).to include(
         {
-          rock_id: a_kind_of(Integer),
+          goal_id: a_kind_of(Integer),
           title: a_kind_of(String),
           meeting_id: a_kind_of(Integer),
           meeting_name: a_kind_of(String),
@@ -53,13 +53,13 @@ RSpec.describe "Rock Operations" do
       )
     end
 
-    it "updates the created rock" do
-      response = @client.rock.update(rock_id: @created_rock[:rock_id], title: "Updated Rock")
+    it "updates the created goal" do
+      response = @client.goal.update(goal_id: @created_goal[:goal_id], title: "Updated Goal")
       expect(response).to include(status: 200)
     end
 
-    it "deletes the created rock" do
-      response = @client.rock.delete(@created_rock[:rock_id])
+    it "deletes the created goal" do
+      response = @client.goal.delete(@created_goal[:goal_id])
       expect(response).to include(status: 200)
     end
   end
