6e409ab5efcfbd85b0a605a9adfce8ed937e926b
Franccesco Orozco <franccesco@codingdose.info>
2024-11-21 17:06:43 -0600

    feat: add data structures for various item types in the application

---
+++ lib/bloomy/types/items.rb
@@ -0,0 +1,129 @@
+# frozen_string_literal: true
+
+TodoItem = Struct.new(
+  :id,
+  :title,
+  :notes_url,
+  :due_date,
+  :created_at,
+  :completed_at,
+  :status,
+  keyword_init: true
+)
+
+HeadlineItem = Struct.new(
+  :id,
+  :title,
+  :notes_url,
+  :meeting_details,
+  :owner_details,
+  :archived,
+  :created_at,
+  :closed_at,
+  keyword_init: true
+)
+
+MeetingItem = Struct.new(
+  :id,
+  :title,
+  :attendees,
+  keyword_init: true
+)
+
+MeetingAttendee = Struct.new(
+  :id,
+  :name,
+  keyword_init: true
+)
+
+MeetingDetails = Struct.new(
+  :id,
+  :title,
+  :attendees,
+  :issues,
+  :todos,
+  :metrics,
+  keyword_init: true
+)
+
+MeetingMetric = Struct.new(
+  :id,
+  :title,
+  :target,
+  :operator,
+  :format,
+  :owner,
+  :admin,
+  keyword_init: true
+)
+
+UserItem = Struct.new(
+  :id,
+  :name,
+  :image_url,
+  :email,
+  :description,
+  :organization_id,
+  :position,
+  :direct_reports,
+  :positions,
+  keyword_init: true
+)
+
+GoalItem = Struct.new(
+  :id,
+  :title,
+  :created_at,
+  :due_date,
+  :status,
+  :meeting_id,
+  :meeting_title,
+  :user_id,
+  :user_name,
+  keyword_init: true
+)
+
+IssueItem = Struct.new(
+  :id,
+  :title,
+  :notes_url,
+  :created_at,
+  :completed_at,
+  :meeting_id,
+  :meeting_title,
+  :user_id,
+  :user_name,
+  keyword_init: true
+)
+
+IssueDetails = Struct.new(
+  :id,
+  :title,
+  :notes_url,
+  :created_at,
+  :completed_at,
+  :meeting_details,
+  :owner_details,
+  keyword_init: true
+)
+
+WeekItem = Struct.new(
+  :id,
+  :week_number,
+  :week_start,
+  :week_end,
+  keyword_init: true
+)
+
+ScorecardItem = Struct.new(
+  :id,
+  :measurable_id,
+  :accountable_user_id,
+  :title,
+  :target,
+  :value,
+  :week,
+  :week_id,
+  :updated_at,
+  keyword_init: true
+)
